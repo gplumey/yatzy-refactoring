9a20d66e4325be1d9dba699fe6b892321243e3b4
gplumey <guillaume.plumey@gmail.com>
2023-12-22 18:06:01 +0100

    refactoring: rename score to value in Score

--- java/src/main/java/org/yatzykata/valueobject/Score.java
+++ java/src/main/java/org/yatzykata/valueobject/Score.java
@@ -1,16 +1,9 @@
 package org.yatzykata.valueobject;
 
-public class Score {
+public record Score(int value) {
 
-    private final int score;
-
-    public Score(int score) {
-        if(score < 0) throw new IllegalStateException("score must be a positive integer.");
-        this.score = score;
-    }
-
-    public int getScore() {
-        return score;
+    public Score {
+        if (value < 0) throw new IllegalStateException("value must be a positive integer.");
     }
 
 
@@ -21,7 +14,7 @@ public class Score {
     @Override
     public String toString() {
         return "Score{" +
-            "score=" + score +
+            "value=" + value +
             '}';
     }
 
@@ -30,7 +23,6 @@ public class Score {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Score score1 = (Score) o;
-        return score == score1.score;
+        return value == score1.value;
     }
-
 }
