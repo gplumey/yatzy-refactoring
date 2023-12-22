3f8d46500e55183a263cafc51f41c8b1ab050b09
gplumey <guillaume.plumey@gmail.com>
2023-12-23 00:26:39 +0100

    perf: store counters of Side in a computed map in the constructor

--- java/src/main/java/org/yatzykata/valueobject/Roll.java
+++ java/src/main/java/org/yatzykata/valueobject/Roll.java
@@ -3,21 +3,26 @@ package org.yatzykata.valueobject;
 import org.yatzykata.error.State;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Roll {
 
     private final List<Side> sides;
+
+    private final Map<Side, Long> countBySideMap;
 
     public Roll(Collection<Side> sides) {
         State.requireNonNull(sides, "sides most not be null");
         if (sides.stream().anyMatch(Objects::isNull)) {
             throw new IllegalStateException("All sides must not be null");
         }
-        this.sides = List.copyOf(sides);
+        this.sides = List.copyOf(sides); // sides is a unmodifiable List
+        countBySideMap = this.sides.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
 
     public Collection<Side> read() {
-        return Collections.unmodifiableList(sides);
+        return sides;
     }
 
 
@@ -33,6 +38,6 @@ public class Roll {
     }
 
     public long countBySide(Side side) {
-        return sides.stream().filter(tmpSide -> tmpSide == side).count();
+        return countBySideMap.getOrDefault(side, 0L);
     }
 }
