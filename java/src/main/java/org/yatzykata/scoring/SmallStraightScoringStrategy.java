67cf3b889174be00a280b2be9cbdbf8bbb0c4b5f
gplumey <guillaume.plumey@gmail.com>
2023-12-23 23:42:08 +0100

    [GREEN]: small straight

--- java/src/main/java/org/yatzykata/scoring/SmallStraightScoringStrategy.java
+++ java/src/main/java/org/yatzykata/scoring/SmallStraightScoringStrategy.java
@@ -2,10 +2,17 @@ package org.yatzykata.scoring;
 
 import org.yatzykata.valueobject.Roll;
 import org.yatzykata.valueobject.Score;
+import org.yatzykata.valueobject.Side;
+
+import java.util.Set;
 
 public class SmallStraightScoringStrategy implements ScoringStrategy {
+    public static Set<Side> SMALL_STRAIGHT_SIDES = Set.of(Side.ONE, Side.TWO, Side.THREE, Side.FOUR, Side.FIVE);
+
     @Override
     public Score score(Roll roll) {
-        return Score.ZERO;
+        var rollSides = Set.copyOf(roll.read());
+        var canReadSmallStraight = rollSides.containsAll(SMALL_STRAIGHT_SIDES);
+        return canReadSmallStraight ? Score.SMALL_STRAIGHT : Score.ZERO;
     }
 }
