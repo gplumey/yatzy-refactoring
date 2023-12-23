9d3c39ca4d55bfa7465cc10b267a74c6cc03fb08
gplumey <guillaume.plumey@gmail.com>
2023-12-23 21:39:04 +0100

    [RED]: score sum highest pair

--- java/src/test/java/org/yatzykata/scoring/PairScoringStrategyTest.java
+++ java/src/test/java/org/yatzykata/scoring/PairScoringStrategyTest.java
@@ -1,14 +1,10 @@
 package org.yatzykata.scoring;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 import org.yatzykata.valueobject.Roll;
 import org.yatzykata.valueobject.Score;
 import org.yatzykata.valueobject.Side;
-
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,16 +16,15 @@ class PairScoringStrategyTest {
         this.pairScoringStrategy = new PairScoringStrategy();
     }
 
-    static Stream<Arguments> score_pairs() {
-        return Stream.of(
-            Arguments.of(Roll.of(Side.ONE, Side.ONE, Side.TWO))
-        );
+    @Test
+    void score_sums_single_pair() {
+        var singlePairRoll = Roll.of(Side.ONE, Side.ONE, Side.TWO);
+        assertEquals(Score.of(2), pairScoringStrategy.score(singlePairRoll));
     }
 
-    @ParameterizedTest
-    @MethodSource
-    void score_pairs(Roll roll) {
-        assertEquals(Score.of(2), pairScoringStrategy.score(roll));
+    @Test
+    void score_sums_highest_pair_when_two_pairs_exists() {
+       var twoPairRoll = Roll.of(Side.TWO, Side.THREE, Side.THREE, Side.TWO);
+        assertEquals(Score.of(6), pairScoringStrategy.score(twoPairRoll));
     }
-
 }
