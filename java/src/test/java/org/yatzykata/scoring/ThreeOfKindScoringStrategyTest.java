95564fa61a64238c85afa38ed1c2777a7a6cc4e2
gplumey <guillaume.plumey@gmail.com>
2023-12-23 22:57:01 +0100

    test: add more tests ThreeOfKindScoringStrategyTest

--- java/src/test/java/org/yatzykata/scoring/ThreeOfKindScoringStrategyTest.java
+++ java/src/test/java/org/yatzykata/scoring/ThreeOfKindScoringStrategyTest.java
@@ -2,9 +2,14 @@ package org.yatzykata.scoring;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.yatzykata.valueobject.Roll;
 import org.yatzykata.valueobject.Score;
 import org.yatzykata.valueobject.Side;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,16 +21,24 @@ class ThreeOfKindScoringStrategyTest {
         this.threeOfKindScoringStrategy = new ThreeOfKindScoringStrategy();
     }
 
-    @Test
-    void score_sums_three_of_a_kind() {
-        var roll = Roll.of(Side.ONE, Side.ONE, Side.ONE);
-        assertEquals(Score.of(3), threeOfKindScoringStrategy.score(roll));
+    static Stream<Arguments> score_sums_three_of_a_kind() {
+        return Stream.of(
+            Arguments.of(Score.of(3), Roll.of(Side.ONE, Side.ONE, Side.ONE)),
+            Arguments.of(Score.of(9), Roll.of(Side.THREE, Side.THREE, Side.THREE)),
+            Arguments.of(Score.of(6), Roll.of(Side.ONE,Side.TWO, Side.TWO, Side.THREE, Side.TWO))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void score_sums_three_of_a_kind(Score expectedScore, Roll roll) {
+        assertEquals(expectedScore, threeOfKindScoringStrategy.score(roll));
     }
 
 
     @Test
     void score_returns_ZERO_when_no_pair_exits() {
-        var noPairRoll = Roll.of(Side.ONE, Side.THREE, Side.TWO, Side.FOUR);
+        var noPairRoll = Roll.of(Side.ONE, Side.THREE, Side.TWO, Side.FOUR, Side.ONE);
         assertEquals(Score.ZERO, threeOfKindScoringStrategy.score(noPairRoll));
     }
 }
