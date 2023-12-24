1556efbdd0246b5b46953582d3883cbbfdab7f5f
gplumey <guillaume.plumey@gmail.com>
2023-12-24 13:50:47 +0100

    BREAKING CHANGE: remove deprecated three_of_a_kind and four_of_a_kind methods

--- java/src/test/java/org/yatzykata/YatzyTest.java
+++ java/src/test/java/org/yatzykata/YatzyTest.java
@@ -234,14 +234,6 @@ public class YatzyTest {
         }
     }
 
-    @Test
-    public void three_of_a_kind_scores_the_sum_of_three_matching_dice_if_exit() {
-        assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 4, 5));
-        assertEquals(15, Yatzy.three_of_a_kind(5, 3, 5, 4, 5));
-        assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 5));
-        assertEquals(0, Yatzy.three_of_a_kind(3, 3, 4, 5, 6));
-        assertEquals(9, Yatzy.three_of_a_kind(3, 3, 3, 3, 3));
-    }
 
     @Nested
     class FourOfKindScore {
@@ -259,15 +251,6 @@ public class YatzyTest {
         void four_of_a_kind_scores_the_sum_of_four_matching_dice_if_exit(Score expectedScore, Roll roll) {
             assertEquals(expectedScore, yatzy.fourOfKing(roll));
         }
-    }
-
-
-    @Test
-    public void four_of_a_kind_scores_the_sum_of_four_matching_dice_if_exit() {
-        assertEquals(12, Yatzy.four_of_a_kind(3, 3, 3, 3, 5));
-        assertEquals(20, Yatzy.four_of_a_kind(5, 5, 5, 4, 5));
-        assertEquals(0, Yatzy.four_of_a_kind(2, 2, 2, 5, 5));
-        assertEquals(8, Yatzy.four_of_a_kind(2, 2, 2, 2, 2));
     }
 
     @Test
